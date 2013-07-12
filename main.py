0e67ded7dd1de39f3789417bdf83552278beca87
Suuria <a.naumann2012@gmail.com>
2013-07-12 17:42:07 +0200

    tests

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 __author__ = 'alex'
 
-import argparse, httplib
+import argparse
+import httplib
+#import httplib2
+
 
 def adddomain(domain):
     print domain
 
+
 def connect(domain):
+    """
+
+    :param domain:
+    """
     c = httplib.HTTPConnection(domain)
     c.request("GET", "/index.php")
     c1 = c.getresponse()
     print c1.getheaders()
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Is the Site Up')
-    parser.add_argument('-d','--domain', dest='domain', help='Add Domain to inspect', required=True)
+    parser.add_argument('-d', '--domain', dest='domain', help='Add Domain to inspect', required=False,
+                        default='shop24direct.de')
     args = parser.parse_args()
 
 connect(args.domain)
