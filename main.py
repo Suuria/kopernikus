cac90936366be48a1bb5de08739ac7dce5f39ade
alex <a.naumann2012@gmail.com>
2013-07-15 18:45:07 +0200

    +add realdomain

--- main.py
+++ main.py
@@ -8,26 +8,21 @@ def adddomain(domain):
     print domain
 
 
-def aim(domain):
-    """
-
-    :param domain:
-    """
-    a = httplib.HTTPConnection(domain)
-    a.request("GET", "/index.php")
-    a1 = a.getresponse()
-    astat = a1.status
-    if (astat == 302) or (astat == 301):
-        a2 = a1.getheader('location')
-        return a2, astat
-    return (domain, astat)
+def realdomain(falsedomain):
+    domain = 0
+    return domain
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Is the Site Up')
-    parser.add_argument('-d', '--domain', dest='domain', help='Add Domain to inspect', required=False,
+    parser.add_argument('-d', '--domain', dest='domain', help='Domain to inspect', required=False,
                         default='www.s24d.de')
     args = parser.parse_args()
 
-c1, c2 = aim(args.domain)
-print c1, c2
+    conn = httplib.HTTPConnection(args.domain)
+    conn.request("GET", "/")
+    conn1 = conn.getresponse()
+    connstat = conn1.status
+
+    if connstat == 301:
+        realdomain(conn1.getheader('location'))
