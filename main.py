6d03fdfe7ef58a3cab68be84e30b627ebcede816
Suuria <a.naumann2012@gmail.com>
2013-07-17 01:01:48 +0200

    +add

--- main.py
+++ main.py
@@ -4,28 +4,10 @@ import argparse
 import httplib
 
 
-def adddomain(domain):
-    print domain
-
-
-def realdomain(falsedomain):
-    print falsedomain
-    print "Your Domain redirects to " + falsedomain
-    rd = raw_input("Would you like to use this Domain? (Y/n) ")
-    if (rd == "Y") or (rd == "Yes"):
-        return falsedomain
-    else:
-        return 0
-    #input = raw_input()
-    #while len(input) == "no":
-    #    print "Must enter 10 characters, each being a-f."
-    #    input = raw_input()
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Is the Site Up')
     parser.add_argument('-d', '--domain', dest='domain', help='Domain to inspect', required=False,
-                        default='www.s24d.de')
+                        default='rewrite.local')
     args = parser.parse_args()
 
     conn = httplib.HTTPConnection(args.domain)
@@ -34,4 +16,12 @@ if __name__ == '__main__':
     connstat = conn1.status
 
     if connstat == 301:
-        realdomain(conn1.getheader('location'))
+        domain = conn1.getheader('location')
+        print "Your Domain redirects to " + domain
+        rd = raw_input("Would you like to use this Domain? (Y/n) ")
+        if (rd == "Y") or (rd == "Yes"):
+            domain = domain
+        else:
+            domain = args.domain
+
+    print domain
